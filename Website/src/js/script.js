94d318a449c4897ef1bf8ae0633cebd7a0297433
Alexander Saga <lexansaga72@gmail.com>
2021-07-27 13:22:46 +0800

    Update Sidebar

--- Website/src/js/script.js
+++ Website/src/js/script.js
@@ -1,94 +1,179 @@
-$(document).ready(function()
-{
-    $("#btn_menu").click(function()
-    {
-        $(".s-navbar").show();
-      //  $('body:not(.s-navbar)').css('filter','blur(4px)').css('opacity','1');
-     
-    });
-    if (performance.navigation.type == 2) {
-        alert("Back button clicked");
-    }
-    $(".profile-picture").click(function()
-    {
-        $(".profile-menu").show();
-    });
-   PieChart();
-   LineChart();
+$(document).ready(function () {
+
+  
+
+    MediaQuery();
+    OnLoadMediaQuery();
+   // DashBoardResponsive();
+    Chart.defaults.global.defaultFontFamily = "Karla";
+    PieChart();
+    LineChart();
 });
 
 
-function PieChart()
-{
-      var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
-        var yValues = [55, 49, 44, 24, 15];
-        var barColors = [
-            "#B14674",
-            "#FE5277",
-            "#FF8D73",
-            "#FFDF9A",
-            "#E1EDFF"
-        ];
 
-        new Chart("pieChart", {
-            type: "pie",
-            data: {
-                labels: xValues,
-                datasets: [{
-                    backgroundColor: barColors,
-                    data: yValues
-                }]
-            },
-            options: {
-                maintainAspectRatio: false,
-                responsive: true,
-                legend: {
-                    position: 'bottom',
-                    display: true,
-                    font: {
-                        family: "'Karla', sans-serif"
-                    }
-                }
-            }
-        });
+
+function OnLoadMediaQuery()
+{
+    var width = $(window).width();
+    var height = $(window).height();
+    if (width > 768) {
+       // alert("Desktop");
+
+
+    } else if (width < 768 && width > 425) {
+        //  alert("Tablet");
+        ChartSizes();
+        DashBoardResponsive();
+
+
+    } else if (width < 425) {
+        //   alert("Mobile");
+        ChartSizes();
+        DashBoardResponsive();
+
+    }
+
 }
-function LineChart()
+function MediaQuery() {
+    $(window).on('resize', function (e) {
+        var width = $(window).width();
+        var height = $(window).height();
+
+        if (width > 768) {
+            // alert("Desktop");
+
+        
+        } else if (width < 768 && width > 425) {
+ 
+            DashBoardResponsive();
+            ChartSizes();
+            //  alert("Tablet");
+          } else if (width < 425) {
+            ChartSizes();
+            //   alert("Mobile");
+        }
+    });
+}
+function ChartSizes()
 {
-       var xValues = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000];
+    var prof = $('.professor').width();
+    $('.pie-chart').css("width", prof);
+    $('.line-chart').css("width", prof);
+}
 
-        new Chart("lineChart", {
-            type: "line",
-            data: {
-                labels: xValues,
-                datasets: [{
-                    data: [860, 1140, 1060, 1060, 1070, 1110, 1330, 2210, 7830, 2478],
-                    borderColor: "#FFDF9A",
-                    backgroundColor: 'rgba(255,223,154,0.2)',
-                    fill: true
-                }, {
-                    data: [1600, 1700, 1700, 1900, 2000, 2700, 4000, 5000, 6000, 7000],
-                    borderColor: "#FE5277",
-                    backgroundColor: "rgba(254, 82, 119,0.2)",
-                    fill: true
-                }, {
-                    data: [300, 700, 2000, 5000, 6000, 4000, 2000, 1000, 200, 100],
-                    borderColor: "#FF8D73",
-                    backgroundColor: "rgba(255, 141, 115,0.2)",
-                    fill: true
-                }],
-                borderWidth: 1
-            },
-            options: {
-                maintainAspectRatio: false,
-                responsive: true,
-                legend: {
-                    position: 'bottom',
-                    display: true,
-                    font: {
-                        family: "'Karla', sans-serif"
-                    }
+
+function PieChart() {
+    var xValues = ["Absent","Present","Late"];
+    var yValues = [Math.floor(Math.random() * 100),
+        Math.floor(Math.random() * 100),
+        Math.floor(Math.random() * 100)];
+    var barColors = [
+        "#B14674",
+        "#FE5277",
+        "#FF8D73",
+        "#FFDF9A",
+        "#E1EDFF"
+    ];
+
+    new Chart("pieChart", {
+        type: "pie",
+        data: {
+            labels: xValues,
+            datasets: [{
+                backgroundColor: barColors,
+                data: yValues
+            }]
+        },
+        options: {
+            maintainAspectRatio: false,
+            responsive: true,
+            legend: {
+                position: 'bottom',
+                display: true,
+                font: {
+                    family: "'Karla', sans-serif"
+                },
+                labels: {
+                    usePointStyle: true
                 }
-
             }
-        });
+        }
+    });
+}
+
+function LineChart() {
+    var xValues = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000];
+
+    new Chart("lineChart", {
+        type: "line",
+        data: {
+            labels: xValues,
+            datasets: [{
+                label:'Absent' ,
+                data: [Math.floor(Math.random() * 8000),
+                    Math.floor(Math.random() * 8000),
+                    Math.floor(Math.random() * 8000), 
+                    Math.floor(Math.random() * 8000),
+                    Math.floor(Math.random() * 8000), 
+                    Math.floor(Math.random() * 8000), 
+                    Math.floor(Math.random() * 8000),
+                    Math.floor(Math.random() * 8000),
+                    Math.floor(Math.random() * 8000),
+                    Math.floor(Math.random() * 8000)],
+                    borderWidth: 1,
+                borderColor: "#FFDF9A",
+                backgroundColor: 'rgba(255,223,154,0.2)',
+                fill: true
+            }, {
+                label:'Present' ,
+                data: [Math.floor(Math.random() * 8000),
+                    Math.floor(Math.random() * 8000),
+                    Math.floor(Math.random() * 8000), 
+                    Math.floor(Math.random() * 8000),
+                    Math.floor(Math.random() * 8000), 
+                    Math.floor(Math.random() * 8000), 
+                    Math.floor(Math.random() * 8000),
+                    Math.floor(Math.random() * 8000),
+                    Math.floor(Math.random() * 8000),
+                    Math.floor(Math.random() * 8000)],
+                    borderWidth: 1,
+                borderColor: "#FE5277",
+                backgroundColor: "rgba(254, 82, 119,0.2)",
+                fill: true
+            }, {
+                label:'Late' ,
+                data: [Math.floor(Math.random() * 8000),
+                    Math.floor(Math.random() * 8000),
+                    Math.floor(Math.random() * 8000), 
+                    Math.floor(Math.random() * 8000),
+                    Math.floor(Math.random() * 8000), 
+                    Math.floor(Math.random() * 8000), 
+                    Math.floor(Math.random() * 8000),
+                    Math.floor(Math.random() * 8000),
+                    Math.floor(Math.random() * 8000),
+                    Math.floor(Math.random() * 8000)],
+                    borderWidth: 1,
+                borderColor: "#FF8D73",
+                backgroundColor: "rgba(255, 141, 115,0.2)",
+                fill: true
+            }],
+            borderWidth: 1
+        },
+        options: {
+            maintainAspectRatio: false,
+            responsive: true,
+            legend: {
+                position: 'bottom',
+                display: true,
+                font: {
+                    family: "'Karla', sans-serif"
+                },
+                labels: {
+                    usePointStyle: true
+                }
+            }
+
+        }
+    });
 }
---
+++ Website/src/js/sidebar.js
@@ -0,0 +1,183 @@
+$(document).ready(function () {
+
+
+    SideBarResponsive();
+    $("#btn_menu").click(function (e) {
+        e.stopPropagation();
+        SideBarResponsive();
+        var navbar = $(".s-navbar");
+        navbar.css("left", "-270");
+        // navbar.addClass("show-menu").show();
+        navbar.css("display", "block");
+        var pointLeft;
+        // if (navbar.hasClass("show-menu")) {
+
+        if (navbar.css("display") == "block") {
+            pointLeft = 0;
+
+
+        } else {
+            pointLeft = -270;
+
+
+        }
+
+        navbar.css("display", "block").animate({
+            left: pointLeft
+        }, {
+            duration: 600,
+            queue: false
+        });
+        ToggleNavbar(e, pointLeft);
+    });
+
+
+    if (performance.navigation.type == 2) {
+        alert("Back button clicked");
+    }
+
+
+
+    $(".profile-picture").click(function (e) {
+         
+        e.stopPropagation();
+        var opacity;
+        var menu = $('.profile-menu');
+        menu.css("opacity", 0);
+        if (menu.hasClass("show-menu")) {
+            //      alert("Has menu");
+            opacity = 0;
+            menu.removeClass("show-menu").animate({
+                opacity: opacity / 10
+            }, {
+                duration: 600,
+                queue: false
+            });
+
+
+        } else {
+            // alert("No menu");
+            opacity = 10;
+            menu.css("top", $(".header").height()).show();
+            menu.addClass("show-menu").animate({
+                opacity: opacity / 10
+            }, {
+                duration: 600,
+                queue: false
+            });
+        }
+
+        ToggleProfile(e, opacity);
+
+    });
+
+
+    MediaQuery();
+    OnLoadMediaQuery();
+    // DashBoardResponsive();
+    Chart.defaults.global.defaultFontFamily = "Karla";
+
+});
+
+
+
+function ToggleNavbar(event, point) {
+    $('body,html').click(function (e) {
+        e.stopImmediatePropagation();
+        var container = $(".s-navbar");
+        //  alert(!menu.is(e.target));
+        if (!container.is(e.target) &&
+            container.has(e.target).length === 0 &&
+            container.css("display") == "block") {
+
+            container.animate({
+                left: -270
+            }, {
+                duration: 500,
+                queue: false
+            });
+            $('body,html').unbind();
+        }
+
+
+    });
+
+}
+
+function ToggleProfile(event, opacity) {
+    $('body,html').click(function (e) {
+      
+        e.stopImmediatePropagation();
+        var menu = $('.profile-menu');
+        //  alert(!menu.is(e.target));
+        if (!menu.is(e.target) &&
+            menu.has(e.target).length === 0 &&
+            menu.has("show-menu")) {
+
+           
+
+            menu.removeClass("show-menu").animate();
+        }
+        $('body,html').unbind();
+
+    });
+}
+
+
+function SideBarResponsive() {
+    var maincontent = $('.main_content');
+    var navbar = $('.s-navbar');
+    //alert(navbar.css("left") == "-270px");
+    if (navbar.css("display") == 'none' || navbar.css("left") == "-270px") {
+        marginLeft = 0;
+        $('#btn_menu').css("visibility", "visible");
+    } else {
+        marginLeft = navbar.width();
+        $('#btn_menu').css("visibility", "hidden");
+    }
+    maincontent.css("margin-left", marginLeft);
+}
+
+function OnLoadMediaQuery() {
+    var width = $(window).width();
+    var height = $(window).height();
+    if (width > 768) {
+        // alert("Desktop");
+
+
+    } else if (width < 768 && width > 425) {
+        //  alert("Tablet");
+
+
+
+    } else if (width < 425) {
+        //   alert("Mobile");
+
+
+    }
+
+}
+
+function MediaQuery() {
+    $(window).on('resize', function (e) {
+        var width = $(window).width();
+        var height = $(window).height();
+
+        if (width > 768) {
+            // alert("Desktop");
+
+            $('body,html').unbind();
+            $('.s-navbar').css("left", "0").animate();
+            $('.profile-menu').css("top", $(".header").height()).animate();
+        } else if (width < 768 && width > 425) {
+            ToggleNavbar(e, -270);
+
+            //  alert("Tablet");
+            $('.s-navbar').css("left", "-270").animate();
+        } else if (width < 425) {
+
+            //   alert("Mobile");
+        }
+        SideBarResponsive();
+    });
+}
