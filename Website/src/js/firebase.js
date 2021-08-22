35f28b9e689cdce2c64b0f21d46f6f4515f108bc
Alexander Saga <lexansaga72@gmail.com>
2021-08-22 11:41:38 +0800

    Firebase Added

--- Website/src/js/firebase.js
+++ Website/src/js/firebase.js
@@ -1,39 +1,31 @@
-var firebaseConfig = {
-    apiKey: "API_KEY",
-    authDomain: "PROJECT_ID.firebaseapp.com",
-    databaseURL: "https://PROJECT_ID.firebaseio.com",
-    projectId: "PROJECT_ID",
-    storageBucket: "PROJECT_ID.appspot.com",
-    messagingSenderId: "SENDER_ID",
-    appId: "APP_ID",
-    measurementId: "G-MEASUREMENT_ID",
-};
+//Initialize Config
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 function GET($PATH) {
-    var query = firebase.database($PATH);
-    query.once("value").then(function (snapshot) {
-        if (snapshot.exists()) {
-            snapshot.foreach(function (childSnapshot) {
-                var key = childSnapshot.key;
-                var childData = childSnapshot.val();
-
-            });
-        } else {
-            console.log("No Data Exists!");
-        }
-
+  
+    var query = firebase.database().ref($PATH);
+    query.on("value", snap => {
+      
+      sessionStorage.setItem("Data", JSON.stringify(snap.val()));
     });
+
+  
+    return JSON.parse(sessionStorage.getItem("Data"));
+
 }
 
-function INSERT($PATH, $DATA) {
-    var query = firebase.database($PATH).update($DATA);
+function SET($PATH, $DATA) {
+    var query = firebase.database().ref($PATH).set($DATA);
 }
 
 function DELETE($PATH) {
-    var query = firebase.database($PATH);
+    var query = firebase.database().ref($PATH);
     query.remove();
 }
 
-function SORT(data,prop) {
+function SORT(data, prop) {
     return new Map([...data.entries()].sort((a, b) => a[1][prop] > b[1][prop]));
 }
+
+console.log(GET("User/USER00001"));
