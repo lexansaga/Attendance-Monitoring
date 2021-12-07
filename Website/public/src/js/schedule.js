6b8124669b19d5ca7526df8d5117e559b412dfc6
Alexander Saga <lexansaga72@gmail.com>
2021-12-07 17:45:45 +0800

    push

--- Website/public/src/js/schedule.js
+++ Website/public/src/js/schedule.js
@@ -1,6 +1,32 @@
 $('.UserInformation_wrapper').css('display', 'none');
 var tableUserSched = $('#userSchedule');
 $(document).ready(function () {
+
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            let uid = user.uid;
+            firebase.database().ref(`User/${uid}/`).once('value', snap => {
+                let Account_Type = snap.child('Account_Type').val();
+                let ID = snap.child('ID').val();
+                let Role = snap.child('Role').val();
+                let UserID = snap.child('UserID').val();
+                let Notification = snap.child('Notification').val();
+                let Permission_Tapin = snap.child('Permission').child('TapIn_First').val()
+    
+    
+                if (Account_Type.includes('Administrator')) {
+                    //window.location.replace("main.html");
+                } else if (Account_Type.includes('Faculty')) {
+                    window.location.replace("main.html");
+                } else if (Account_Type.includes('Guidance')) {
+                    window.location.replace("main.html");
+                } else { // Else
+                    window.location.replace("index.html");
+                }
+            })
+        }
+    })
+
     $('.js-example-basic-single').select2();
     $('#look').select2({
         width: '90%',
