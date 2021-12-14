9143e401ecdd746eda4c4cd35c12604aaae32654
Mitch Serrano <ser.mitch2k@gmail.com>
2021-12-14 22:47:24 +0800

    added about and 404 page

---
+++ Website/src/js/about.js
@@ -0,0 +1,46 @@
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
+
+$( document ).ready(function() {
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            let uid = user.uid;
+            firebase.database().ref(`User/${uid}/`).once('value', snap => {
+                let Account_Type = snap.child('Account_Type').val();
+                let ID = snap.child('ID').val();
+                let Role = snap.child('Role').val();
+                let UserID = snap.child('UserID').val();
+                let Notification = snap.child('Notification').val();
+                let Permission_Tapin = snap.child('Permission').child('TapIn_First').val()
+
+
+                if (Account_Type.includes('Administrator')) {
+                   // window.location.replace("main.html");
+                } else if (Account_Type.includes('Faculty')) {
+                   // window.location.replace("main.html");
+                } else if (Account_Type.includes('Guidance')) {
+                    //window.location.replace("main.html");
+                } else { // Else
+                    window.location.replace("index.html");
+                }
+            })
+        }
+    })
+});
